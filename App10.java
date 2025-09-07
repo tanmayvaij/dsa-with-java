0427b07bbd7a7ffb4cafb3e838118481157a8015
Tanmay Vaij <tanmayvaij22@gmail.com>
2025-09-07 21:18:33 +0530

    added a string builder code
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

---
+++ App10.java
@@ -0,0 +1,12 @@
+public class App10 {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("hello");
+
+        sb.setCharAt(0, 'l');
+
+        System.out.println(sb);
+    }
+
+}
